8cd79149e0bcef4cf095661cee89879566c63bb5
unrealJuanpa <juanpagramajo@icloud.com>
2024-06-05 20:48:22 -0600

    Added: Net structure

---
+++ README.md
@@ -0,0 +1,11 @@
+Dataset struct:
+
+1- Mean of the integrated profile, 
+2- Standard deviation of the integrated profile, 
+3- Excess kurtosis of the integrated profile, 
+4- Skewness of the integrated profile, 
+5- Mean of the DM-SNR curve, 
+6- Standard deviation of the DM-SNR curve, 
+7- Excess kurtosis of the DM-SNR curve, 
+8- Skewness of the DM-SNR curve,
+9 (class) - target_class
--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <torch/torch.h>
 #include <iostream>
 
+// Arch
+/*
+  8 inputs
+  2 outputs (softmax, represents 2 classes)
+  
+  3 layers:
+  8 -> 16
+  16 -> 32
+  32 -> 2
+*/
+
+struct Net : torch::nn::Module {
+  Net() {
+    fc1 = register_module("fc1", torch::nn::Linear(8, 16));
+    fc2 = register_module("fc2", torch::nn::Linear(16, 32));
+    fc3 = register_module("fc3", torch::nn::Linear(32, 2));
+  }
+
+  torch::Tensor forward(torch::Tensor x) {
+    x = torch::relu(fc1 -> forward(x));
+    x = torch::relu(fc2 -> forward(x));
+    x = torch::softmax(fc3 -> forward(x), /*dim=*/1);
+    return x;
+  }
+
+  torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
+};
+
+
 int main() {
-  torch::Tensor tensor = torch::rand({2, 3});
-  std::cout << tensor << std::endl;
+  auto net = std::make_shared<Net>();
+  std::cout << "Todo fino!" << std::endl;
 }
